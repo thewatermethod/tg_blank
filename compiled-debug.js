e50af196662070e1457ab6977701533c7b7a7ad9
Matt <thewatermethod@gmail.com>
2017-12-29 15:09:26 -0500

    home page template

--- compiled-debug.js
+++ compiled-debug.js
@@ -68,6 +68,29 @@ function insertSearch( selectorString ){
 
     parentElement.appendChild(searchForm);
 }
+
+
+jQuery(document).ready(function() { 
+
+    (function (jQuery) { 
+        jQuery('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+        
+        jQuery('.tab ul.tabs li a').click(function (g) { 
+            var tab = jQuery(this).closest('.tab'), 
+                index = jQuery(this).closest('li').index();
+            
+            tab.find('ul.tabs > li').removeClass('current');
+            jQuery(this).closest('li').addClass('current');
+            
+            tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+            tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+            
+            g.preventDefault();
+        } );
+    })(jQuery);
+
+});
+
 /**
  * File navigation.js.
  *
--- tmpl-home.php
+++ tmpl-home.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area home-tmpl">
 		<main id="main" class="site-main" role="main">
 
 			<?php 
@@ -32,10 +32,126 @@ get_header(); ?>
 					);
 					
 					$testimonials = get_posts( $testimonial_args );
+					?>
 
-					var_dump( $testimonials);
+					<h2>What Customers Have Said About Us</h2>
+					<div class="testimonials">
+						
+						<?php
 
+						foreach ($testimonials as $testimonial) { ?>
+						
+							<div class="testimonial">
+								<div class="quote">
+									<p><?php echo $testimonial->post_content; ?></p>
+								</div>
+								<div class="customer">
+									<h3><?php echo $testimonial->post_title; ?></h3>
+								</div>
+							</div>					
+
+					<?php
+						}
+					?></div><?php
 				}
+
+			//
+			
+			if( have_rows('services_layer') ): 
+				$tab_titles = array();
+				$tab_descriptions = array();
+				$tab_thumbnails = array();
+			?>
+
+				<h2>Services We Offer</h2> <?php
+
+				while( have_rows('services_layer') ): the_row();
+					
+					$tab_titles[] = get_sub_field('title');
+					$tab_description[] = get_sub_field('description');
+					$tab_thumbnails[] = get_sub_field('thumbnail');						
+						
+				endwhile; ?>
+
+				<ul class="tabs active">
+				
+				<?php
+
+				foreach ( $tab_titles as $tab_title ) { ?>
+					<li><?php echo $tab_title; ?></li>
+				<?php 
+				}
+				?>
+
+				<div class="tab_content">
+
+					<?php 
+						foreach ($tab_titles as $key => $tab_title) {
+							
+							?>
+
+					<div class="tabs_item">
+					
+						<h3><?php echo $tab_title; ?></h3>
+
+						<?php if( $tab_thumbnails[ $key ] ) : ?>
+							<img alt="" src="<?php echo $tab_thumbnails[ $key ];?>"> 
+						<?php endif;
+						
+						echo $tab_descriptions[$key]; 
+						
+					?></div>
+							
+					<?php
+						} ?>
+				
+					</div>
+
+
+	<?php	endif;
+
+			if( have_rows('content_section') ):
+
+				while( have_rows('content_section') ): the_row();
+					
+					$bg_color = get_sub_field('background_color');
+					$bg_image = get_sub_field('background_image');
+					$content = get_sub_field('content');
+
+				?>
+
+					<div class="full-content <?php echo $bg_color;?>">
+						<?php echo $content; ?>					
+					</div>
+
+			<?php
+				endwhile;
+
+			endif;
+
+			if( have_rows('services_summary') ):
+
+				?><div class="services-summary"><?php
+
+				while( have_rows('services_summary') ): the_row(); 
+
+					$title = get_sub_field('title');
+					$summary = get_sub_field('summary');
+				
+				?>
+
+					<div>
+						<h3><?php echo $title; ?></h3>
+						<p><?php echo $summary; ?></p>
+					</div>
+			
+				<?php 
+				
+				endwhile;
+
+				?></div><?php
+
+			endif;
 
 			?>
 
